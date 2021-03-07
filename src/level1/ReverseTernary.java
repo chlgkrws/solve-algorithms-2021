f9cd82177e5ef8b9e2529fda6a2aa2b448a17966
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-03-07 23:26:45 +0900

    Update ReverseTernary

--- src/level1/ReverseTernary.java
+++ src/level1/ReverseTernary.java
@@ -8,10 +8,13 @@ public class ReverseTernary {
 	 * https://programmers.co.kr/learn/courses/30/lessons/68935
 	 */
 	public int solution(int n) {
+		//1. 10진수를 3진수로 바꿔준다.
+		//2. 바꾼 3진수를 뒤집는다.
+		//3. 10진수로 바꿔준다.
 		String rTernary = "";
 		
 		while(n >= 3) {											//3진법 변환
-			rTernary += (n % 3) + "";
+			rTernary += (n % 3) + "";							
 			n /= 3;
 		}
 		rTernary += n;											//마지막 몫 더해주기.
