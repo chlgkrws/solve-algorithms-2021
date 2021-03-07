a7d860835f8fa16b1c8c6961eb9f5828ff0ed7a7
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-03-07 23:12:37 +0900

    Create Note

---
+++ src/level1/Note.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class Note {
+
+	public static void main(String[] args) {
+		int n = 45;
+		String radix = ""; 
+		
+		while(n >= 3) {
+			
+		}
+		System.out.println(Integer.parseInt("0021",3));
+		
+	}
+
+}
