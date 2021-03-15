1e551ef94c6323f275ff00c3c975238b71f2a496
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-03-15 22:23:36 +0900

    브라이언의 고민

---
+++ src/level3/BriansWorry.java
@@ -0,0 +1,21 @@
+package level3;
+
+public class BriansWorry {
+	
+	/**
+	 * level 3
+	 * 브라이언의 고민
+	 * https://programmers.co.kr/learn/courses/30/lessons/1830
+	 */
+	public String solution(String sentence) {
+		//단어 사이마다 소문자가 올 수 있음.
+		//단어를 소문자가 감쌀 수 있음.
+		//두 규칙은 한단어에 동시에 존재 할 수 있음.
+		return "t";
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
