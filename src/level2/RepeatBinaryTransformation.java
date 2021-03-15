8e04f4ca4daff460bc9d8b01284e869588bcf6f3
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-03-15 22:26:02 +0900

    이진 변환 반복하기

--- src/level2/RepeatBinaryTransformation.java
+++ src/level2/RepeatBinaryTransformation.java
@@ -11,18 +11,23 @@ public class RepeatBinaryTransformation {
 		int convertCount = 0;
 		int removeZero = 0;
 		
+		//s의 길이가 1이 아니면서
 		while(!s.equals("1")) {
 			int zero = 0;
+			
+			//0 개수 구하기.
 			for(int i = 0; i < s.length(); i++) {
 				if(s.charAt(i) == '0') zero++;
 			}
 			
+			//0을 제거한 뒤, 해당 문자열의 길이를 이진수로 변환. ex 1111(4) -> 100
 			s = s.replaceAll("0", "");
 			s = Integer.toBinaryString(s.length());
 			convertCount++;
 			removeZero += zero;
 		}
  		
+		//변환한 스텝과 제거된 0의 개수를 배열로 반환
 		return new int[] {convertCount, removeZero};
 	}
 	public static void main(String[] args) {
