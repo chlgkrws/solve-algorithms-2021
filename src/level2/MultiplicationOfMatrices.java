51b4ff7b9e3d7ee48002084c01a38da285cb2598
it1457 <cgw981@naver.com>
2021-06-28 20:53:42 +0900

    행렬의 곱셈

--- src/level2/MultiplicationOfMatrices.java
+++ src/level2/MultiplicationOfMatrices.java
@@ -14,11 +14,9 @@ public class MultiplicationOfMatrices {
 		
 		for(int i = 0; i < arr1.length; i++) {
 			for(int j = 0; j < arr2[0].length; j++) {
-				int temp = 0;
 				for(int k = 0; k < arr2.length; k++) {
-					temp += arr1[i][k] + arr2[k][j];
+					matrix[i][j] += arr1[i][k] * arr2[k][j];
 				}
-				matrix[i][j] = temp; 
 			}
 		}
 		
